f9bcfb23f07e81a51e362439676326beb94f9849
yahohulia <yaroslavhohulia@gmail.com>
2025-10-03 14:56:42 +0300

    updated logic

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -8,13 +8,15 @@
 function convertToObject(sourceString) {
   return sourceString
     .split(';')
-    .map((object) => object.trim())
-    .filter((object) => object !== undefined)
-    .filter((object) => object.length > 0)
-    .reduce((acc, object) => {
-      const [key, value] = object.split(':');
+    .map((splitString) => splitString.trim())
+    .filter((keys) => keys !== undefined)
+    .filter((keys) => keys.length > 0)
+    .reduce((acc, declaration) => {
+      const [key, value] = declaration.split(':');
 
-      acc[key.trim()] = value.trim();
+      if (key) {
+        acc[key.trim()] = value.trim();
+      }
 
       return acc;
     }, {});
